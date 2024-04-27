e33fe4d64e0a535e892182f9c9aeb847e53738a4
lu- <lu->
2024-04-27 22:35:59 +0300

    l

--- my_order/items/views.py
+++ my_order/items/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Страница моего порядка")
+
+
+def categories(request, catid):
+    return HttpResponse(f"<h1> Категория {catid}</h1>")
