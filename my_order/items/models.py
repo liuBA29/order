7c2fbe43cd37732a6c5b7b807cf2b60cb2efd77b
lu- <lu->
2024-05-05 21:54:34 +0300

    staring2

--- my_order/items/models.py
+++ my_order/items/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 
+
+class Furniture(models.Model):
+    slug = models.SlugField(max_length=25, db_index=True, unique=True, verbose_name='furniture')
+    details = models.CharField(max_length=115)
+    photo = models.ImageField(upload_to="photos/furniture/%Y/%m/", blank=True, null=True)
+
+    def __str__(self):
+        return self.slug
+
+class TheItem(models.Model):
+    name = models.CharField(max_length=35)
+    location = models.ForeignKey(Furniture, max_length=25, blank=True, null=True, on_delete=models.PROTECT,
+                                 verbose_name="Furniture of the location", related_name="location")
+
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Time create")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="time of last update")
+    photo = models.ImageField(upload_to="photos/items/%Y/%m/")
+
+    def __str__(self):
+        return self.name
